9861c5ff6b6aa08b6ab8cfd629697bcfb88af6e7
Rukhmini123 <rukhminideshmukh590@gmail.com>
2022-07-28 12:22:01 +0530

    First commit

---
+++ src/pkg1/Class1.java
@@ -0,0 +1,55 @@
+package pkg1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		
+
+		int a = 10;
+		int b = 20;     //arithmetic operators= +,-,/,*,%
+		
+		int c = a + b;
+		
+		System.out.println(c);
+		
+            c = a - b;
+		
+		System.out.println(c);
+		
+            c = a * b;
+		
+		System.out.println(c);
+
+            c = a / b;
+		
+		System.out.println(c);
+		
+            c = a % b;
+		
+		System.out.println(c);
+		
+		c= 30;
+
+	
+	//LOGICAL OPERATORS= &&, ||, !=
+		
+     if(a < b && !(b < c))
+     {
+    	 
+    	 System.out.println("b is greater than a");
+     }else
+    	 
+     {
+    	 
+    	 System.out.println("c is the highest number");
+     }
+	
+     
+     // RELATIONAL OPERATORS= <, >, ==, <=,>=
+     
+     
+	}
+
+
+
+}
